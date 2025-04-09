77b3614a25403d1811af7176da62ec5dce763b29
Student <student@b426-3.intranet.cs.upt.ro>
2025-04-09 15:32:14 +0300

    done lab7

--- maven-hello-world-starter-main (1)/src/main/java/org/example/Main.java
+++ maven-hello-world-starter-main (1)/src/main/java/org/example/Main.java
@@ -1,7 +1,51 @@
+//package org.example;
+
+//public class Main {
+//    public static void main(String[] args) {
+//       System.out.println("Hello world!");
+//   }
+//}
+
+/*package org.example;
+
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.AnsiConsole;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AnsiConsole.systemInstall();
+        System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("Hello World!").reset());
+        AnsiConsole.systemUninstall();
+    }
+}*/
+
 package org.example;
+
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.AnsiConsole;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AnsiConsole.systemInstall();
+
+        String language;
+
+        if (args.length == 0) {
+            language = "en";
+        } else {
+            language = args[0].toLowerCase();
+        }
+        Greeting gp = new Greeting(language);
+
+        System.out.println(Ansi.ansi()
+                .fg(gp.getGreetingColor()).a(gp.getGreeting() + " ")
+                .fg(gp.getPersonColor()).a(gp.getPerson() + " ")
+                .fg(gp.getPunctuationColor()).a(gp.getPunctuation())
+                .reset()
+        );
+
+        AnsiConsole.systemUninstall();
     }
 }
+
